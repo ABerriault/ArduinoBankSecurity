f3f09183182fa8cd035c98a0c8b27dea904e2fba
Andrew Berriault <berriaak@mcmaster.ca>
2017-07-28 19:34:31 -0400

    Updated for new breadboard configuration.
    Still having mild problems with the servo motor.

--- demo/demo.ino
+++ demo/demo.ino
@@ -38,24 +38,24 @@ void setup() {
   lcd.print("hello, world!");
 
   // setup for the servo motor
-  servo.attach(5);
+  servo.attach(6);
 
   // setup for the speaker
   pinMode(4, OUTPUT);
-  pinMode(6, INPUT);
+  pinMode(5, INPUT);
 
   // setup for the LED strip
   pixels.begin();
   pixels.show();
 
-  
+
   // setup for serial terminal
   Serial.begin(9600);
 }
 
 void loop() {
   // draw a pattern on the LED strip
-  one_two_blink();
+  comet();
 
   // write number of seconds since startup on the LCD
   lcd.setCursor(0, 1);
@@ -63,19 +63,20 @@ void loop() {
 
   // read from the photoresistor, make a sound on the buzzer if it is pressed
   int read_value = analogRead(0);
-  int button_pressed = digitalRead(6);
-  if (read_value < 200 || button_pressed) {
+  int button_pressed = digitalRead(5);
+  if (read_value < 75 || button_pressed) {
     digitalWrite(4, HIGH);
   } else {
     digitalWrite(4, LOW);
   }
 
   // print the value read from the photoresistor
-  Serial.println(read_value);
-
+  //Serial.println(read_value);
   servo.write(servo_position);
   if (servo_position++ > 180) {
     servo_position = 0;
   }
 
+  // print the value read from the servo
+  //Serial.println(servo_position);
 }
